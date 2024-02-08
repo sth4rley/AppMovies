16c520401c5ccd948673279f366a3ecba922e5e6
Stharley <sstharley@gmail.com>
2024-02-07 22:40:09 -0300

    added favorites, watched and watchlist

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.kt
@@ -12,8 +12,8 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.BuildConfig
+import com.example.myapplication.MovieDetailsActivity
 import com.example.myapplication.ui.ItemAdapter
-import com.example.myapplication.ui.MovieDetailsActivity
 import com.example.myapplication.api.MovieResponse
 import com.example.myapplication.api.TmdbApiService
 import com.example.myapplication.databinding.FragmentHomeBinding
